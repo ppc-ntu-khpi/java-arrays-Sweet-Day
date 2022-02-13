248366e150a6d81e74d52ac61935747d9a9fa29f
Duminike Vika <75427769+Sweet-Day@users.noreply.github.com>
2022-02-13 13:51:18 +0000

    done

---
+++ src/TestResult.java
@@ -0,0 +1,12 @@
+//package test;
+
+//import domain.Exercise;
+
+public class TestResult {
+
+    public static void main(String[] args) {
+
+       int [] arr ={5,5,5,5,5,6,4,7,8};
+        System.out.println("The element that is repeated most number of times: " + Exercise.Calculate(arr));
+    }
+}
